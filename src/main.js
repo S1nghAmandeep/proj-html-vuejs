fad31f70aaa4a17509a835b7614fd77a09a358ad
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-15 19:38:02 +0100

    add-icons

--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
-import { faBagShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faBagShopping, faMagnifyingGlass, faGaugeHigh, faCloud, faTabletScreenButton, faStopwatch } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faHeart, faBagShopping, faMagnifyingGlass);
+library.add(faHeart, faBagShopping, faMagnifyingGlass, faGaugeHigh, faCloud, faTabletScreenButton, faStopwatch);
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
