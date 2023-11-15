b3dd2ca7e3f42d5cdcfd06032682d5ab0f73c69f
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-15 14:59:51 +0100

    done-nav-bar

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -5,12 +5,55 @@ export default {
 
     data() {
         return {
-
+            navBar: ['Home', 'About', 'Services', 'Showcase', 'Blog', 'Contact']
         };
     }
 }
 </script>
 
-<template></template>
+<template>
+    <header class="container">
+        <nav class="row nav-bar">
+            <div>
+                <img class="nav-logo" src="/cropped-Group-39-2x.png" alt="">
+            </div>
+            <div>
+                <ul class="row nav-items">
+                    <li v-for="item in navBar"><a href="#">{{ item }}</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul class="row nav-items">
+                    <li><button class="button">Purchase</button></li>
+                    <li class="search-icon"><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../styles/parstials/variables' as*;
+
+.nav-bar {
+    padding: 20px 0;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.nav-logo {
+    height: 40px;
+}
+
+.nav-items {
+    display: flex;
+    gap: 30px;
+    align-items: center;
+    text-transform: uppercase;
+    font-size: 14px;
+}
+
+.search-icon {
+    color: $bg-green;
+}
+</style>
