85e64d2e46463c86b1d9b67e3ab3526e62fd56f2
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-15 11:47:46 +0100

    add-scss-file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup>
+<script>
 // import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template></template>
+<template>
+  <h1>HELLO WORLD</h1>
+  <p>ciao come stari?</p>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+@use './styles/general.scss'
+</style>
