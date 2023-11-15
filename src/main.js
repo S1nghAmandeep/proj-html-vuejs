377893d4d8e79dbd35968cb35d8700220d91ade2
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-15 11:18:44 +0100

    scaffolding

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
