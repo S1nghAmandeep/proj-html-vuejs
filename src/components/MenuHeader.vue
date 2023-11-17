25b3f6fb0d8af982e2879e2fe969b8920df72e23
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-17 15:20:07 +0100

    add-menu-header-component

---
+++ src/components/MenuHeader.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    props: {
+        menu: {
+            type: Array,
+            required: true
+        },
+    },
+
+    data() {
+        return {
+
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <ul v-for="link in menu">
+            <li><a :href="link.href">{{ link.text }}</a></li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
