2cd7b9f56eb5f2abbda5d85e9d338be6b9fd1d2b
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-17 11:32:20 +0100

    add-footer-menu-component

---
+++ src/components/MenuFooter.vue
@@ -0,0 +1,34 @@
+<script>
+
+export default {
+    props: {
+        menu: {
+            type: Array,
+            required: true
+        },
+
+        title: {
+            type: String,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h4 class="menu-title">{{ title }}</h4>
+        <ul v-for="link in menu">
+            <li><a :href="link.href">{{ link.text }}</a></li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
